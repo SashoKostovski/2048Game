d0293ef4bbc9fddfbddc14b334df47471b08ed32
Sasho Kostovski <saso.kostovski49@gmail.com>
2024-07-07 18:29:45 +0200

    implementation of the application

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBest = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(379, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnUp.Location = new System.Drawing.Point(139, 241);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(100, 100);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.TabStop = false;
+            this.btnUp.Text = "W";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnLeft.Location = new System.Drawing.Point(33, 356);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(100, 100);
+            this.btnLeft.TabIndex = 5;
+            this.btnLeft.TabStop = false;
+            this.btnLeft.Text = "A";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDown.Location = new System.Drawing.Point(139, 356);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(100, 100);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.TabStop = false;
+            this.btnDown.Text = "S";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRight.Location = new System.Drawing.Point(245, 356);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(100, 100);
+            this.btnRight.TabIndex = 7;
+            this.btnRight.TabStop = false;
+            this.btnRight.Text = "D";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRestart.Location = new System.Drawing.Point(34, 515);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(307, 100);
+            this.btnRestart.TabIndex = 8;
+            this.btnRestart.TabStop = false;
+            this.btnRestart.Text = "Restart (Space)";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(956, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Score:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblScore.Location = new System.Drawing.Point(956, 118);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(26, 29);
+            this.lblScore.TabIndex = 10;
+            this.lblScore.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(1099, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Best:";
+            // 
+            // lblBest
+            // 
+            this.lblBest.AutoSize = true;
+            this.lblBest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblBest.Location = new System.Drawing.Point(1099, 118);
+            this.lblBest.Name = "lblBest";
+            this.lblBest.Size = new System.Drawing.Size(26, 29);
+            this.lblBest.TabIndex = 12;
+            this.lblBest.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(444, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "00:00";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(375, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Time:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReset.Location = new System.Drawing.Point(1070, 570);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(171, 74);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset Session";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 656);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblBest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ WindowsFormsApp1/board.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace WindowsFormsApp1
+{
+    internal class board
+    {
+        public List<Zone> zones;
+
+        public int zcount {get { return zones.Count;} }
+        public int elWidth {  get; set; }
+        public int score { get; set; }
+
+        public int bScore { get; set; } = 0;
+
+        
+        public void calcScore()
+        {
+            double s = 0, coef = 0.1;
+            foreach(Zone z in zones)
+            {
+                if (z.value >= 2048)
+                {
+                    coef = 0.03;
+                }
+                else if (z.value >= 1024)
+                {   
+                    coef = 0.05;
+                }
+                else if(z.value >=512)
+                {
+                    coef = 0.07;
+                }
+                s += z.value * (z.value * coef);
+                score = (int)s;
+            }
+            if(score > bScore)
+            {
+                bScore = score;
+            }
+        }
+        public board() { 
+            zones = new List<Zone>();
+          
+            zones.Add(new Zone(11));
+            zones.Add(new Zone(12));
+            zones.Add(new Zone(13));
+            zones.Add(new Zone(14));
+            zones.Add(new Zone(21));
+            zones.Add(new Zone(22));
+            zones.Add(new Zone(23));
+            zones.Add(new Zone(24));
+            zones.Add(new Zone(31));
+            zones.Add(new Zone(32));
+            zones.Add(new Zone(33));
+            zones.Add(new Zone(34));
+            zones.Add(new Zone(41));
+            zones.Add(new Zone(42));
+            zones.Add(new Zone(43));
+            zones.Add(new Zone(44));
+
+        }
+      
+
+        public void drawBack(Graphics g)
+        {
+            SolidBrush br = new SolidBrush(Color.FromArgb(100, 100, 100));
+            g.FillRectangle(br, 0, 0, 350, 350);
+
+            br = new SolidBrush(Color.FromArgb(200, 200, 200));
+            g.FillRectangle(br,  2, 2 , 346, 346);
+            br.Dispose();
+        }
+
+        public void drawAll(Graphics g)
+        {
+
+            foreach (Zone frm in zones)
+                frm.draw(g);
+        }
+    }
+}
